d231da116d36a79862f32005d4c7f3cfafed7d60
Simon Sagstetter <44363600+simonsagstetter@users.noreply.github.com>
2021-01-25 21:15:42 +0100

    Update routing.py

--- messenger/routing.py
+++ messenger/routing.py
@@ -2,6 +2,8 @@ from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter, URLRouter
 import chats.routing
 
+app_name = 'routing'
+
 application = ProtocolTypeRouter({
     'websocket': AuthMiddlewareStack(
         URLRouter(
